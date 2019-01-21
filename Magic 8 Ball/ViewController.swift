55abefda815b03e7e47bec1cdf6a4c356690ad09
Ratnam Yadav <ratnam.yadav@icloud.com>
2019-01-21 08:07:56 +0530

    Magic ball completed

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Variables
+    let ballImages = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomBallNumber:Int = 0;
+    // UI components
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setRandomBall()
     }
 
-
+    @IBAction func askButton(_ sender: UIButton) {
+        setRandomBall()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            setRandomBall()
+        }
+    }
+    
+    func setRandomBall() {
+        randomBallNumber = Int.random(in: 0...4)
+        imageView.image = UIImage(named: ballImages[randomBallNumber])
+    }
 }
 
